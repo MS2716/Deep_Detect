10ed9c2dcd0887310765c95ab3ab4259d3ae02fa
Maitri Suthar <135234149+MS2716@users.noreply.github.com>
2025-05-05 12:19:44 +0530

    Update firebase.ts

--- frontend/app/config/firebase.ts
+++ frontend/app/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCP4gnlRyLtq-AgGmA6-qUpKwg9ccNq4GQ",
-    authDomain: "deepdetect-f9fb8.firebaseapp.com",
-    projectId: "deepdetect-f9fb8",
-    storageBucket: "deepdetect-f9fb8.firebasestorage.app",
-    messagingSenderId: "726950523019",
-    appId: "1:726950523019:web:c95e1fce355a75f234a48e",
-    measurementId: "G-QVHYS5X3PG"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase only once
